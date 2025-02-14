9a0b4c731db52c9f5b619158e5ef361a9fb265f7
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-02-14 22:54:36 +0530

    deployment changes made

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://flirt-ninja-chat-app--nine.vercel.app/',
     credentials:true
 };
 app.use(cors(corsOption)); 
